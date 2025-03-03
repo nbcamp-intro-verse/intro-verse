d08ba0bbe2b82c06faf706dd6eba2da6fa5b6622
youseokhwan <youseokhwan15@gmail.com>
2025-03-03 21:33:22 +0900

    feat: Dummy 버튼 추가하여 각 VC에 연결

--- Intro-verse/Presentation/Main/MainViewController.swift
+++ Intro-verse/Presentation/Main/MainViewController.swift
@@ -6,6 +6,69 @@ final class MainViewController: UIViewController {
     private var cards: [Card] = []
     private var activateCard: Card?
 
+    /**
+     Dummy UI Components
+     */
+    lazy var jiSungButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("JiSung", for: .normal)
+        button.addTarget(self, action: #selector(jiSungAction), for: .touchUpInside)
+        return button
+    }()
+    lazy var minJaeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("MinJae", for: .normal)
+        button.addTarget(self, action: #selector(minJaeAction), for: .touchUpInside)
+        return button
+    }()
+    lazy var NickButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Nick", for: .normal)
+        button.addTarget(self, action: #selector(nickAction), for: .touchUpInside)
+        return button
+    }()
+    lazy var seokHwanButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("SeokHwan", for: .normal)
+        button.addTarget(self, action: #selector(seokHwanAction), for: .touchUpInside)
+        return button
+    }()
+    lazy var seoYoungButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("SeoYoung", for: .normal)
+        button.addTarget(self, action: #selector(seoYoungAction), for: .touchUpInside)
+        return button
+    }()
+
+    /**
+     Dummy Button Actions
+     */
+    @objc func jiSungAction() {
+        let viewController = JiSungViewController()
+        viewController.modalPresentationStyle = .automatic
+        present(viewController, animated: true)
+    }
+    @objc func minJaeAction() {
+        let viewController = MinJaeViewController()
+        viewController.modalPresentationStyle = .automatic
+        present(viewController, animated: true)
+    }
+    @objc func nickAction() {
+        let viewController = NickViewController()
+        viewController.modalPresentationStyle = .automatic
+        present(viewController, animated: true)
+    }
+    @objc func seokHwanAction() {
+        let viewController = SeokHwanViewController()
+        viewController.modalPresentationStyle = .automatic
+        present(viewController, animated: true)
+    }
+    @objc func seoYoungAction() {
+        let viewController = SeoYoungViewController()
+        viewController.modalPresentationStyle = .automatic
+        present(viewController, animated: true)
+    }
+
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -44,10 +107,53 @@ final class MainViewController: UIViewController {
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
+//        setupView()
+        dummySetupView()
     }
 
     // MARK: - Methods
+    /**
+     Dummy Methods
+     */
+    private func dummySetupView() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(jiSungButton)
+        view.addSubview(minJaeButton)
+        view.addSubview(NickButton)
+        view.addSubview(seokHwanButton)
+        view.addSubview(seoYoungButton)
+        jiSungButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+        }
+        minJaeButton.snp.makeConstraints { make in
+            make.top.equalTo(jiSungButton.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(220)
+            make.height.equalTo(50)
+        }
+        NickButton.snp.makeConstraints { make in
+            make.top.equalTo(minJaeButton.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(220)
+            make.height.equalTo(50)
+        }
+        seokHwanButton.snp.makeConstraints { make in
+            make.top.equalTo(NickButton.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(220)
+            make.height.equalTo(50)
+        }
+        seoYoungButton.snp.makeConstraints { make in
+            make.top.equalTo(seokHwanButton.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(220)
+            make.height.equalTo(50)
+        }
+    }
+
     private func setupView() {
         view.addSubview(backgroundImageView)
         view.addSubview(blurView)
