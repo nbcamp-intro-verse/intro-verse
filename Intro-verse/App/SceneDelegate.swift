a7ac6772c85ae74be0218f8c1fbc9e33d421ea06
youseokhwan <youseokhwan15@gmail.com>
2025-03-03 20:19:22 +0900

    refactor: 불필요한 주석 삭제

--- Intro-verse/App/SceneDelegate.swift
+++ Intro-verse/App/SceneDelegate.swift
@@ -1,18 +1,13 @@
-//
-//  SceneDelegate.swift
-//  Intro-verse
-//
-//  Created by Seungho Jang on 3/3/25.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
-    func scene(_ scene: UIScene,
-               willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         self.window = UIWindow(windowScene: windowScene)
--- Intro-verse/Presentation/ViewController.swift
+++ Intro-verse/Presentation/ViewController.swift
@@ -1,21 +1,10 @@
-//
-//  ViewController.swift (Main View)
-
 import UIKit
 import Combine
 
 class ViewController: UIViewController {
-    
-    // MARK: - Property
     private var cards: [Card] = []
     private var activateCard: Card?
-    
-    // _ as indicator for private (can be excluded)
-    // collection view
-    // reason why I chose: collection view
-    // * https://www.reddit.com/r/swift/comments/4gh5ta/question_help_should_i_use_collection_view_or/
-    // * https://stackoverflow.com/questions/23078847/when-to-use-uicollectionview-instead-of-uitableview
-    // More Customizable, both are inherited from UIScrollView
+
     private lazy var collectionView_: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -29,7 +18,7 @@ class ViewController: UIViewController {
         collectionView.register(CarouselCardCell.self, forCellWithReuseIdentifier: "CarouselCardCell")
         return collectionView
     }()
-    
+
     private lazy var backgroundImgView_: UIImageView = {
         let imgView = UIImageView()
         imgView.translatesAutoresizingMaskIntoConstraints = false
@@ -37,38 +26,32 @@ class ViewController: UIViewController {
         imgView.clipsToBounds = true
         return imgView
     }()
-    
-    // To set the blur view
-    // https://zeddios.tistory.com/1140
+
     private lazy var blurView_: UIVisualEffectView = {
         let blurEffect = UIBlurEffect(style: .dark)
         let blurView = UIVisualEffectView(effect: blurEffect)
         blurView.translatesAutoresizingMaskIntoConstraints = false
         return blurView
     }()
-    
-    init(){
-        // Maybe setup view model?
+
+    init() {
         super.init(nibName: nil, bundle: nil)
     }
-    
-    // Incase, init(frame: ) is called
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
-    
+
     private func setupView() {
         view.addSubview(backgroundImgView_)
         view.addSubview(blurView_)
         view.addSubview(collectionView_)
-        
-        // Constraints
+
         NSLayoutConstraint.activate([
             backgroundImgView_.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             backgroundImgView_.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
@@ -85,31 +68,27 @@ class ViewController: UIViewController {
             collectionView_.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             collectionView_.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.45)
         ])
-        
     }
-    
-    // MARK: - Bind View Model
+
     private func bindViewModel() {
     }
-    
-    // MARK: - Update UI View
+
     private func updateUIView(width card: Card?) {
+
     }
-    
-    // COMMENT: Not Decided Yet
 }
 
-extension ViewController : UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return cards.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         return UICollectionViewCell()
     }
-    
+
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
+
     }
 }
 
