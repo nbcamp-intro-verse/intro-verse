36e800df40a43fa8b6aea64c984b1e3c85e27b2f
youseokhwan <youseokhwan15@gmail.com>
2025-03-03 20:23:15 +0900

    refactor: fatalError 코드 삭제

--- Intro-verse/Presentation/MainViewController.swift
+++ Intro-verse/Presentation/MainViewController.swift
@@ -39,7 +39,7 @@ final class MainViewController: UIViewController {
     }
 
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 
     override func viewDidLoad() {
@@ -71,7 +71,7 @@ final class MainViewController: UIViewController {
     }
 
     private func bindViewModel() {
-        
+
     }
 
     private func updateUIView(width card: Card?) {
