7409bb6a1a1973c6c3163442545a716adacabeae
youseokhwan <youseokhwan15@gmail.com>
2025-03-05 00:15:17 +0900

    feat: keywordsView 구현

--- IntroVerse/Presentation/SeokHwan/SeokHwanViewController.swift
+++ IntroVerse/Presentation/SeokHwan/SeokHwanViewController.swift
@@ -4,12 +4,16 @@ import SnapKit
 final class SeokHwanViewController: UIViewController {
     private let viewModel = SeokHwanViewModel()
 
-    private lazy var scrollView = UIScrollView()
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isScrollEnabled = true
+        return scrollView
+    }()
     private lazy var contentView = UIView()
     private lazy var profileImageView = SeokHwanProfileImageView()
     private lazy var headerView = SeokHwanHeaderView()
     private lazy var dividerView = SeokHwanDividerView()
-    private lazy var keywordsView = SeokHwanKeywordsView()
+    private lazy var keywordsStackView = SeokHwanKeywordsStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +37,7 @@ private extension SeokHwanViewController {
         contentView.addSubview(profileImageView)
         contentView.addSubview(headerView)
         contentView.addSubview(dividerView)
-        contentView.addSubview(keywordsView)
+        contentView.addSubview(keywordsStackView)
     }
 
     func configureConstraints() {
@@ -41,7 +45,7 @@ private extension SeokHwanViewController {
             make.edges.equalTo(view.safeAreaLayoutGuide)
         }
         contentView.snp.makeConstraints { make in
-            make.horizontalEdges.width.equalToSuperview()
+            make.edges.width.equalToSuperview()
         }
         profileImageView.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(30)
@@ -51,17 +55,18 @@ private extension SeokHwanViewController {
         headerView.snp.makeConstraints { make in
             make.top.equalTo(profileImageView.snp.bottom).offset(30)
             make.horizontalEdges.equalToSuperview().inset(20)
-            make.height.equalTo(220)
+            make.height.equalTo(200)
         }
         dividerView.snp.makeConstraints { make in
             make.top.equalTo(headerView.snp.bottom).offset(20)
             make.width.equalToSuperview().inset(20)
             make.centerX.equalToSuperview()
         }
-        keywordsView.snp.makeConstraints { make in
+        keywordsStackView.snp.makeConstraints { make in
             make.top.equalTo(dividerView.snp.bottom).offset(20)
             make.width.equalToSuperview().inset(20)
             make.centerX.equalToSuperview()
+            make.bottom.equalTo(contentView.snp.bottom).inset(20)
         }
     }
 }
---
+++ IntroVerse/Presentation/SeokHwan/Subviews/SeokHwanKeywordView.swift
@@ -0,0 +1,52 @@
+import UIKit
+import SnapKit
+
+final class SeokHwanKeywordView: UIView {
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.borderWidth = 1
+        imageView.layer.cornerRadius = 10
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(systemName: "house.circle")
+        return imageView
+    }()
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.text = "textLabel"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    convenience init() {
+        self.init(frame: .zero)
+        configure()
+    }
+}
+
+private extension SeokHwanKeywordView {
+    func configure() {
+        configureLayout()
+        configureConstraints()
+    }
+
+    func configureLayout() {
+        addSubview(imageView)
+        addSubview(textLabel)
+    }
+
+    func configureConstraints() {
+        snp.makeConstraints { make in
+            make.height.equalTo(60)
+        }
+        imageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.width.height.equalTo(60)
+        }
+        textLabel.snp.makeConstraints { make in
+            make.leading.equalTo(imageView.snp.trailing).offset(20)
+            make.trailing.equalToSuperview()
+            make.centerY.equalTo(imageView.snp.centerY)
+        }
+    }
+}
