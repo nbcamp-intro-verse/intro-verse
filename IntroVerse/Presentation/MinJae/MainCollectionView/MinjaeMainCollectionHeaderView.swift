53b42b66d5120cc59cb86f26a3693f65184523ea
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-05 17:20:04 +0900

    Merge branch 'feature/MinjaeView' into dev

---
+++ IntroVerse/Presentation/MinJae/MainCollectionView/MinjaeMainCollectionHeaderView.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+protocol MinjaeCollectionHeaderViewDelegate: AnyObject {
+    func linkButtonTapped(url: URL)
+}
+
+final class MinjaeMainCollectionHeaderView: UICollectionReusableView {
+    static let identifier = "MinjaeMainCollectionHeaderView"
+    
+    weak var delegate: MinjaeCollectionHeaderViewDelegate?
+    private lazy var githubButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "GithubIcon"), for: .normal)
+        btn.backgroundColor = .black
+        btn.layer.cornerRadius = 10
+        btn.addTarget(self, action: #selector(githubButtonTapped), for: .touchUpInside)
+        return btn
+    }()
+    
+    private lazy var notionButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "NotionIcon"), for: .normal)
+        btn.backgroundColor = .black
+        btn.layer.cornerRadius = 10
+        btn.addTarget(self, action: #selector(notionButtonTapped), for: .touchUpInside)
+        return btn
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(githubButton)
+        addSubview(notionButton)
+        configureLayout()
+        backgroundColor = .white.withAlphaComponent(0)
+    }
+    
+    @objc func githubButtonTapped() {
+        print("github")
+        delegate?.linkButtonTapped(url: URL(string: "https://github.com/minjae-L")!)
+    }
+    
+    @objc func notionButtonTapped() {
+        print("notion")
+        delegate?.linkButtonTapped(url: URL(string: "https://global-profit-946.notion.site/iOS-17feb1b45ca080cb9bb1ce6def7306b3")!)
+    }
+    
+    private func configureLayout() {
+        notionButton.snp.makeConstraints { make in
+            make.width.height.equalTo(40)
+            make.trailing.equalToSuperview().inset(15)
+            make.bottom.equalToSuperview().inset(10)
+        }
+        githubButton.snp.makeConstraints { make in
+            make.width.height.equalTo(40)
+            make.bottom.equalToSuperview().inset(10)
+            make.trailing.equalTo(notionButton.snp.leading).offset(-10)
+        }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- IntroVerse/Presentation/MinJae/MinJaeViewController.swift
+++ IntroVerse/Presentation/MinJae/MinJaeViewController.swift
@@ -1,22 +1,59 @@
 import UIKit
 import SnapKit
+import SafariServices
 
 final class MinJaeViewController: UIViewController {
+    private let mainCollectionView = MinjaeMainCollectionView()
     private var viewModel = MinJaeViewModel()
-
-    private lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "MinJae"
-        label.textAlignment = .center
-        return label
+    
+    private lazy var backgroundImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        return iv
     }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        view.addSubview(titleLabel)
-        titleLabel.snp.makeConstraints { make in
-            make.edges.equalTo(view.safeAreaLayoutGuide)
+    private func addViews() {
+        view.addSubview(mainCollectionView)
+    }
+    
+    private func configureLayout() {
+        mainCollectionView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+        backgroundImageView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview()
         }
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainCollectionView.collectionViewDelegate = self
+        view.addSubview(backgroundImageView)
+        loadImage(url: URL(string: Constant.imageURL)!)
+        addViews()
+        configureLayout()
+    }
+    
+    private func loadImage(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.backgroundImageView.image = image
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension MinJaeViewController: MinjaeMainCollectionViewDelegate {
+    func linkButtonTapped(url: URL) {
+        DispatchQueue.main.async { [weak self] in
+            let safariView = SFSafariViewController(url: url)
+            self?.present(safariView, animated: true)
+        }
+    }
+}
+
+struct Constant {
+    static var imageURL = "https://github.com/user-attachments/assets/7083f9cd-4418-4107-937e-e6c56659e255"
 }
