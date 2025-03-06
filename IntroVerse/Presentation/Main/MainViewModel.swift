a87c7f70e593a738f4a2370285061d64e5f8ee9b
이민재 <qlqlzbto@naver.com>
2025-03-06 19:09:39 +0900

    feat: 그림라벨 추가

--- IntroVerse/Presentation/Main/MainViewModel.swift
+++ IntroVerse/Presentation/Main/MainViewModel.swift
@@ -2,7 +2,7 @@ import Foundation
 
 final class MainViewModel {
     let cards: [Card] = [
-        Card(name: "이민재", memberType: .memberMan, birthday: "19000101", imageName: ""),
+        Card(name: "이민재", memberType: .memberMan, birthday: "19000101", imageName: "minjaeProfile"),
         Card(name: "박지성", memberType: .leader, birthday: "19000101", imageName: "jisung_profile"),
         Card(name: "장승호", memberType: .memberMan, birthday: "19000101", imageName: "nick_profile"),
         Card(name: "한서영", memberType: .memberWoman, birthday: "19000101", imageName: "sy_profile"),
