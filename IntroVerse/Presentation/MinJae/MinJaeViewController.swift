bd45e59961832d383f5c73ca8c13f7635bb3228f
이민재 <qlqlzbto@naver.com>
2025-03-06 17:59:10 +0900

    feat: Minjae Image 에셋 추가 및 적용

--- IntroVerse/Presentation/MinJae/MinJaeViewController.swift
+++ IntroVerse/Presentation/MinJae/MinJaeViewController.swift
@@ -1,27 +1,16 @@
 import UIKit
 import SnapKit
-import Combine
 import SafariServices
 
 final class MinJaeViewController: UIViewController {
     private let mainCollectionView = MinjaeMainCollectionView()
     private var viewModel = MinJaeViewModel()
-    private let imageRepository: ImageRepository
-    private var cancellables = Set<AnyCancellable>()
-    
-    init(imageRepository: ImageRepository) {
-        self.imageRepository = imageRepository
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
     
     private lazy var backgroundImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFill
-        return iv
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "minjaeProfile")
+        return imageView
     }()
     
     private func addViews() {
@@ -40,7 +29,6 @@ final class MinJaeViewController: UIViewController {
         super.viewDidLoad()
         mainCollectionView.collectionViewDelegate = self
         view.addSubview(backgroundImageView)
-        loadImage(url: URL(string: Constant.imageURL)!)
         addViews()
         configureLayout()
         
@@ -64,20 +52,6 @@ final class MinJaeViewController: UIViewController {
             self.present(alertController, animated: true)
         }
     }
-    
-    private func loadImage(url: URL) {
-        imageRepository.loadImageNetwork(from: url, cahceOption: .memory)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                if case .failure(let error) = completion {
-                    print("Failed to load image: \(error)")
-                }
-            } receiveValue: { [weak self] image in
-                self?.backgroundImageView.image = image
-                self?.mainCollectionView.reloadData()
-            }
-            .store(in: &cancellables)
-    }
 }
 
 extension MinJaeViewController: MinjaeMainCollectionViewDelegate {
@@ -87,8 +61,4 @@ extension MinJaeViewController: MinjaeMainCollectionViewDelegate {
             self?.present(safariView, animated: true)
         }
     }
-}
-
-struct Constant {
-    static var imageURL = "https://github.com/user-attachments/assets/7083f9cd-4418-4107-937e-e6c56659e255"
 }
