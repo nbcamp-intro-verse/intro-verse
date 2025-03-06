f6a54ebc54b18ee80c7775c180dc0f38b7b1a183
youseokhwan <youseokhwan15@gmail.com>
2025-03-06 16:58:18 +0900

    refactor: UIImage resize extensions 파일로 이동

---
+++ IntroVerse/Utils/UIImage+Extensions.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+extension UIImage {
+    func resize(targetSize: CGSize) -> UIImage? {
+        let newRect = CGRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newRect.size, true, 0)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        context.interpolationQuality = .high
+        draw(in: newRect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+}
