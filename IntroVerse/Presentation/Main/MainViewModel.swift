f8f4903812360f7b600266ec6ddd799867c73d39
이민재 <qlqlzbto@naver.com>
2025-03-06 18:01:29 +0900

    feat: Card Minjae Image name 추가

--- IntroVerse/Presentation/Main/MainViewModel.swift
+++ IntroVerse/Presentation/Main/MainViewModel.swift
@@ -7,7 +7,7 @@ final class MainViewModel {
         Card(name: "장승호", memberType: .memberMan, birthday: "19000101", imageName: "nick_profile"),
         Card(name: "한서영", memberType: .memberWoman, birthday: "19000101", imageName: "sy_profile"),
         Card(name: "유석환", memberType: .memberMan, birthday: "19000101", imageName: "SeokHwanProfile"),
-        Card(name: "이민재", memberType: .memberMan, birthday: "19000101", imageName: ""),
+        Card(name: "이민재", memberType: .memberMan, birthday: "19000101", imageName: "minjaeProfile"),
         Card(name: "박지성", memberType: .leader, birthday: "19000101", imageName: "jisung_profile"),
     ]
     let selectedCardIndex = 0
